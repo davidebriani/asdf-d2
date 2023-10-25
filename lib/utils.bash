e5bf3fb84a79e6d797cddcf8ae5784b650d03e5e
Davide Briani <davide@briani.dev>
2023-10-25 22:36:04 +0200

    Configure installation script
    Adapt default template to list and download releases from the GitHub
    repository of D2.
    
    Signed-off-by: Davide Briani <davide@briani.dev>

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for d2.
 GH_REPO="https://github.com/terrastruct/d2"
 TOOL_NAME="d2"
 TOOL_TEST="d2 --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if d2 has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,8 +38,24 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for d2
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	case $(uname -s) in
+	Darwin) os="macos" ;;
+	Windows) os="windows" ;;
+	*) os="linux" ;;
+	esac
+
+	case $(uname -m) in
+	x86_64) arch="amd64" ;;
+	aarch64) arch="arm64" ;;
+	*) arch="other" ;;
+	esac
+
+	if [ "$arch" = "other" ]; then
+		echo "Unsupported architecture $(uname -m). Only x86_64 and ARM binaries are available."
+		exit
+	fi
+
+	url="$GH_REPO/releases/download/${version}/d2-${version}-${os}-${arch}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -59,9 +72,8 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH"/bin/* "$install_path"
 
-		# TODO: Assert d2 executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
